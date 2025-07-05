623095f1476e16813894d5f7134eb4a807f58361
s-0-u-l-z <ttvpro2612@gmail.com>
2025-07-05 00:22:40 -0400

    Create FE-Bird-Poop.lua

---
+++ Troll/FE-Bird-Poop.lua
@@ -0,0 +1,32 @@
+--FE BIRD
+--Made by rouxhaver | Modified and repaired by Hugo
+
+----CONTROLS-----------------------------------
+--W, A, S, D, Q, E - to move around and up/down
+--CONTROL - to sprint
+--X - to poop
+-------------------------------------------------
+
+--Required Hats:
+----[FREE]----
+--https://www.roblox.com/catalog/2956239660/Belle-Of-Belfast-Long-Red-Hair
+--https://www.roblox.com/catalog/301818806/Serenas-Hair
+--https://www.roblox.com/catalog/62724852/Chestnut-Bun
+--https://www.roblox.com/catalog/3302593407/Rodans-Head
+--https://www.roblox.com/catalog/253151806/The-Bird-Says
+
+----[PAID]----
+--https://www.roblox.com/catalog/15301922285/Seagull-Head
+--https://www.roblox.com/catalog/17179460188/Paper-Mache-Poop
+--https://www.roblox.com/catalog/5677350668/The-Right-Holy-Wing
+--https://www.roblox.com/catalog/5677348945/The-Left-Holy-Wing
+--https://www.roblox.com/catalog/12331913497/Faceless-Head-White
+
+
+----------[SETTINGS]---------
+RanAsLoadstring = true    --Do not modify this value
+PoopTime = 5              --The time in seconds how long the poop stays on the ground
+NormalSpeed = 0.7         --Set to the normal flying speed
+SprintSpeed = 2           --Set to the speed when you are sprinting
+------------[END]------------
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Windows7King/scripts/main/FE_BIRD.lua"))()
