1b54cf88c6694671bc04638f57bec1f390b76024
s-0-u-l-z <ttvpro2612@gmail.com>
2025-07-04 21:02:58 -0400

    Update Rizz.lua

--- Global/Rizz.lua
+++ Global/Rizz.lua
@@ -1,6 +1,3 @@
---[[
-	WARNING: Heads up! This script has not been verified by ScriptBlox. Use at your own risk!
-]]
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
 local Window = Rayfield:CreateWindow({
