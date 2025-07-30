495a3f4684e33621309fbfb639edf8be34ed728a
s-0-u-l-z <ttvpro2612@gmail.com>
2025-07-30 12:12:23 -0400

    Update and rename aimbot-etc.lua to ZephyrV2.lua
    The Best script

---
+++ Games/MVSD/ZephyrV2.lua
@@ -0,0 +1,1 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/TheRealAvrwm/Zephyr-V2/refs/heads/main/ZephyrV2", true))()
--- Games/MVSD/aimbot-etc.lua
+++
@@ -1,1 +0,0 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/TheRealAvrwm/Projects/main/Xeno%20MVSD%20script.lua", true))()
