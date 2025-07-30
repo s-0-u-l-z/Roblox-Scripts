171310696a12297e885a20ea46924ade63bad4fd
s-0-u-l-z <ttvpro2612@gmail.com>
2025-07-30 12:43:51 -0400

    Update and rename CatBypasser-(Discontinued).lua to CatBypasser(Maintenance).lua

---
+++ Bypassers/CatBypasser(Maintenance).lua
@@ -0,0 +1,1 @@
+print("CatBypasser is being redeveloped releases Aug 1st!")
--- Bypassers/CatBypasser-(Discontinued).lua
+++
@@ -1,1 +0,0 @@
-settings={name='SmeoBl',color='Blue'}setfenv(loadstring(game:HttpGet("https://raw.githubusercontent.com/shadow62x/catbypass/main/upfix")),getfenv())()
