56c42a7a98c178f88d4955a45c06ef22824d9f9c
s-0-u-l-z <ttvpro2612@gmail.com>
2025-08-02 14:41:53 -0400

    Create R6-Animation-Hub.lua

---
+++ Animations/R6-Animation-Hub.lua
@@ -0,0 +1,10 @@
+--[[
+  Version: 1
+  Made by: Fsploit/Eternal_w1nteriris
+
+
+  Credit to the netless owner.
+  Join our discord (.gg/getfrost)
+]]--
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Fsploit/FeAnimationHub/refs/heads/main/Hub.lua"))()
