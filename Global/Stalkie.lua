7d99fe8bc5595cc22f5f014b066ec51d1bf756d1
s-0-u-l-z <ttvpro2612@gmail.com>
2025-08-14 10:09:11 -0400

    Create Stalkie.lua

---
+++ Global/Stalkie.lua
@@ -0,0 +1,9 @@
+--USE AT YOUR OWN RISK
+--JOIN MY SERVER!! https://discord.gg/Nzy9gzSFYM
+ 
+repeat task.wait() until game.Players.LocalPlayer
+loadstring(game:HttpGet("https://raw.githubusercontent.com/0riginalWarrior/Stalkie/refs/heads/main/roblox.lua"))()
+ 
+--Current Key: pizza
+ 
+--Their Discord: https://discord.com/invite/N9yJV6357C
