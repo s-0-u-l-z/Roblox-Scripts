e68a374b2c1fb6f49f94d84d215128b98bb80c93
s-0-u-l-z <ttvpro2612@gmail.com>
2025-08-15 19:36:50 -0400

    Create Sentinel.lua

---
+++ Global/Sentinel.lua
@@ -0,0 +1,6 @@
+-- My server: https://discord.gg/Nzy9gzSFYM
+ 
+-- Made by @fuckluau
+loadstring(game:HttpGet("https://sentinelxyz.netlify.app/raw.lua"))()
+ 
+-- Their server: https://discord.gg/WxczCDGSFc
