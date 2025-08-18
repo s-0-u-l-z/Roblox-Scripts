057840d937ccd4990565f19aa1d51deb5a606bd6
s-0-u-l-z <ttvpro2612@gmail.com>
2025-08-17 20:20:02 -0400

    Update DarkMoon-Hub-OP.lua

--- Games/Egg Toss/DarkMoon-Hub-OP.lua
+++ Games/Egg Toss/DarkMoon-Hub-OP.lua
@@ -1,4 +1,1 @@
---[[
-	WARNING: Heads up! This script has not been verified by ScriptBlox. Use at your own risk!
-]]
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Darkmoonxhubscript/New_scripts/refs/heads/main/hub.lua"))()
