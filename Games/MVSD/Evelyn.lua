b6b737c5e134a8a3ec4edd31314050af4ba12367
s-0-u-l-z <ttvpro2612@gmail.com>
2025-08-30 23:43:34 -0400

    Update Evelyn.lua

--- Games/MVSD/Evelyn.lua
+++ Games/MVSD/Evelyn.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/evelynnscripts/Evelynn-Hub/refs/heads/main/mvsd.lua",true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/evelynnscripts/Scripts/refs/heads/main/Loader.lua",true))()
